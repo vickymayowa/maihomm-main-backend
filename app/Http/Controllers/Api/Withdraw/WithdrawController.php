7dc7a0c87c46bc74f4df638ca68648d791d78796
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-10-05 11:45:21 +0100

    Worked on the withdrawal payment service

--- app/Http/Controllers/Api/Withdraw/WithdrawController.php
+++ app/Http/Controllers/Api/Withdraw/WithdrawController.php
@@ -30,40 +30,45 @@ class WithdrawController extends Controller
 {
 
     const WITHDRAWAL_CURRENCY = CurrencyConstants::EURO_CURRENCY;
+    public function withdraw(Request $request)
+    {
 
-
-    public function withdraw(Request $request) {
-
-        
         DB::beginTransaction();
         try {
-            
+
+            // Validate the request
             $validator = Validator::make($request->toArray(), [
                 "amount" => "bail|required|numeric|gt:0",
                 "password" => "bail|required",
             ]);
-    
+
             if ($validator->fails()) {
                 throw new ValidationException($validator);
             }
 
+            // Check if the provided password is correct
             if (!Hash::check($request->password, auth()->user()->password)) {
                 throw new UserException("Invalid credentials");
             }
 
+            // Fetch the authenticated user
+            $user = User::find(auth()->id());
+
+
+            if ($request->amount > $user->balance) {
+                throw new UserException("Insufficient balance");
+            }
 
             $currency = CurrencyService::getByType(self::WITHDRAWAL_CURRENCY);
 
-            $user = User::find(auth()->id());
-
             $data = [
-                'amount' => "(".self::WITHDRAWAL_CURRENCY.") {$request->amount}"
+                'amount' => "(" . self::WITHDRAWAL_CURRENCY . ") {$request->amount}"
             ];
-
             Notification::route('mail', AppConstants::SUDO_EMAIL)->notify(new WithdrawNotification($data, $user));
-            
+
+            // Create the withdrawal transaction
             UserTransactionService::create([
-                "user_id" => 4,
+                "user_id" => $user->id,
                 "currency_id" => $currency->id,
                 "amount" => $request->amount,
                 "fees" => 0,
@@ -73,20 +78,19 @@ class WithdrawController extends Controller
                 "type" => TransactionConstants::CREDIT,
                 "status" => StatusConstants::PENDING
             ]);
-            
-            
-            // DB::commit();
+
+            // Commit the transaction
+            DB::commit();
+
+            // Return success response
             return ApiHelper::validResponse("Successful. Pending Approval!");
         } catch (ValidationException $e) {
-            // throw $e;
             return ApiHelper::inputErrorResponse("Invalid data", ApiConstants::VALIDATION_ERR_CODE, null, $e);
-        } catch (UserException $e){  
-            return ApiHelper::inputErrorResponse("Invalid password", ApiConstants::VALIDATION_ERR_CODE, null);
+        } catch (UserException $e) {
+            return ApiHelper::inputErrorResponse($e->getMessage(), ApiConstants::VALIDATION_ERR_CODE, null);
         } catch (Exception $e) {
             DB::rollBack();
-            return ApiHelper::problemResponse("Something went wrong while processing your request", ApiConstants::BAD_REQ_ERR_CODE, null,  $e);
+            return ApiHelper::problemResponse("Something went wrong while processing your request", ApiConstants::BAD_REQ_ERR_CODE, null, $e);
         }
-
-        
     }
 }
